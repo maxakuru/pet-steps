d2d17e811c5d63896ad6039a86a56da4ff7f4901
Max Edell <medell@uwo.ca>
2017-02-09 22:17:42 -0500

    ugly, working

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,10 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	results: 0,
+	isDwarf: false,
+	petLegFactor: 0.5,
+	humanLegFactor: 0.415,
+	inchToCm: 2.54,
+	dwarfFactor: 0.6,
 	actions: {
 		calculate: function(){
-			console.log('in calculate: ', this.height)
+			//estimate human's stride length
+			var humanStrideLength = this.humanHeight*this.get('humanLegFactor');
+			
+			//estimate pet's stride length
+			var petStrideLength = this.get('isDwarf') ? 
+			this.petHeight*this.get('petLegFactor')*this.get('dwarfFactor') : 
+			this.petHeight*this.get('petLegFactor');
+
+			//get ratio of human stride length to pet stride length
+			var stepRatio = humanStrideLength/petStrideLength;
+
+			//multiply ratio by distance
+			this.set('results', stepRatio*this.distance);
 		}
 	}
 });
